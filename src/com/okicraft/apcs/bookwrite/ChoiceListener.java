dcc652d640ec6b1089f2b2a93d5920c59c730dd0
adminmip <mip@loyloy.io>
2016-05-16 07:52:19 -0400

    Initial commit

---
+++ src/com/okicraft/apcs/bookwrite/ChoiceListener.java
@@ -0,0 +1,37 @@
+package com.okicraft.apcs.bookwrite;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Created by skitt on 5/10/2016.
+ */
+public class ChoiceListener implements MouseListener {
+
+// MouseListener Methods
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+}
---
+++ src/com/okicraft/apcs/bookwrite/SoundPlayer.java
@@ -0,0 +1,17 @@
+package com.okicraft.apcs.bookwrite;
+
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by skitt on 5/10/2016.
+ */
+public class SoundPlayer {
+
+/*    URL soundURL = this.getClass().getClassLoader().getResource("resouces/keysound.wav");
+    AudioInputStream audioIn =  AudioSystem.getAudioInputStream( soundURL );*/
+    
+}
